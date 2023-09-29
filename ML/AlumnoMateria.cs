35e85d84bea5dd9201eba9304265f56cd3643d5c
MSandoval00 <msandovalg00@gmail.com>
2023-09-28 20:35:10 -0600

    Crud asignar materias

---
+++ ML/AlumnoMateria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class AlumnoMateria
+    {
+        public int IdAlumno { get; set; }
+        public int IdMateria { get; set; }
+    }
+}
---
+++ PL/Controllers/AlumnoMateriaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class AlumnoMateriaController : Controller
+    {
+        // GET: AlumnoMateria
+        public ActionResult GetAll()
+        {
+            ML.Result result=BL.Alumno.GetAll();
+            ML.Alumno alumno=new ML.Alumno();
+            alumno.Alumnos=new List<object>();
+            if (result.Correct)
+            {
+                alumno.Alumnos=result.Objects.ToList();
+            }
+            else
+            {
+                ViewBag.Mensaje = "No hay registros en la tabla";
+            }
+
+            return View(alumno);
+        }
+    }
+}
